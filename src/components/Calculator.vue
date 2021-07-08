ba8a7835ddaf4f9f14ef34abd2919379f6e9f134
Bovo01 <47921869+Bovo01@users.noreply.github.com>
2021-07-08 17:41:45 +0200

    Iniziata visualizzazione su display

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <!-- Display -->
-    <span class="row header"></span>
+    <div class="row">
+      <input class="display" v-model="text" readonly />
+    </div>
     <!-- Bottoni -->
     <div>
       <!-- Prima riga (vars,ops,view mat,/) -->
@@ -9,34 +11,34 @@
         <q-btn>VARS</q-btn>
         <q-btn>OPS</q-btn>
         <q-btn>VIEW MAT</q-btn>
-        <q-btn icon="fas fa-divide" />
+        <q-btn @click="appendText('/')" icon="fas fa-divide" />
       </div>
       <!-- Seconda riga (7,8,9,*) -->
       <div class="row no-wrap">
-        <q-btn>7</q-btn>
-        <q-btn>8</q-btn>
-        <q-btn>9</q-btn>
-        <q-btn icon="fas fa-times" />
+        <q-btn @click="appendText(7)">7</q-btn>
+        <q-btn @click="appendText(8)">8</q-btn>
+        <q-btn @click="appendText(9)">9</q-btn>
+        <q-btn @click="appendText('*')" icon="fas fa-times" />
       </div>
       <!-- Terza riga (4,5,6,-) -->
       <div class="row no-wrap">
-        <q-btn>4</q-btn>
-        <q-btn>5</q-btn>
-        <q-btn>6</q-btn>
-        <q-btn icon="fas fa-minus" />
+        <q-btn @click="appendText(4)">4</q-btn>
+        <q-btn @click="appendText(5)">5</q-btn>
+        <q-btn @click="appendText(6)">6</q-btn>
+        <q-btn @click="appendText('-')" icon="fas fa-minus" />
       </div>
       <!-- Quarta riga (1,2,3,+) -->
       <div class="row no-wrap">
-        <q-btn>1</q-btn>
-        <q-btn>2</q-btn>
-        <q-btn>3</q-btn>
-        <q-btn icon="fas fa-plus" />
+        <q-btn @click="appendText(1)">1</q-btn>
+        <q-btn @click="appendText(2)">2</q-btn>
+        <q-btn @click="appendText(3)">3</q-btn>
+        <q-btn @click="appendText('+')" icon="fas fa-plus" />
       </div>
       <!-- Quinta riga (c,0,<=,=) -->
       <div class="row no-wrap">
-        <q-btn icon="fas fa-trash" />
-        <q-btn>0</q-btn>
-        <q-btn icon="fas fa-backspace" />
+        <q-btn @click="clear()" icon="fas fa-trash" />
+        <q-btn @click="appendText(0)">0</q-btn>
+        <q-btn @click="backspace()" icon="fas fa-backspace" />
         <q-btn icon="fas fa-equals" />
       </div>
     </div>
@@ -48,6 +50,43 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Calculator",
+  data() {
+    return {
+      operations: [],
+    };
+  },
+  computed: {
+    text: function () {
+      return this.operations.join("");
+    },
+  },
+  methods: {
+    appendText(text) {
+      text = String(text);
+      let lastOperation = this.operations[this.operations.length - 1];
+      if (!isNaN(text) && !isNaN(lastOperation)) {
+        if (lastOperation.length < 16)
+          this.operations[this.operations.length - 1] = String(
+            parseInt(lastOperation + text)
+          );
+      } else this.operations.push(text);
+      console.log(this.operations);
+    },
+    clear() {
+      this.operations = [];
+    },
+    backspace() {
+      let lastOperation = this.operations[this.operations.length - 1];
+      if (isNaN(lastOperation) || lastOperation.length <= 1)
+        this.operations.pop();
+      else {
+        this.operations[this.operations.length - 1] = lastOperation.substring(
+          0,
+          lastOperation.length - 1
+        );
+      }
+    },
+  },
 });
 </script>
 
@@ -58,10 +97,14 @@ export default defineComponent({
   margin-right: 10vw;
   margin-bottom: 10vh;
 }
-.header {
+.display {
   border: 2px solid black;
   border-radius: 7px;
   height: 15vh;
+  width: 80vw;
+  cursor: default;
+  outline-width: 0;
+  font-size: min(4vw, 4vh);
 }
 .row {
   margin-top: 3vh;
