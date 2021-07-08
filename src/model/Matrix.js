09ded3d9d3631dc87f11f86363c32acf33bceb19
Bovo01 <47921869+Bovo01@users.noreply.github.com>
2021-07-08 14:49:44 +0200

    Aggiunti i commenti per la classe Matrix

--- src/model/Matrix.js
+++ src/model/Matrix.js
@@ -28,6 +28,12 @@ export default class Matrix {
     this.cols = cols;
   }
 
+  /**
+   * Funzione logicamente privata che permette di creare una nuova matrice in base alle righe inserite (le colonne non sono necessarie)
+   * 
+   * @param {Number} [rows=this.rows] Il numero di righe che deve avere la nuova matrice
+   * @returns La matrice (array di array, non oggetto di classe Matrix) inizializzata con gli array vuoti all'interno
+   */
   _createMatrix(rows = this.rows) {
     let temp = [];
     for (let i = 0; i < rows; i++)
@@ -35,6 +41,11 @@ export default class Matrix {
     return temp;
   }
 
+  /**
+   * Funzione logicamente privata che permette di copiare la matrice dell'oggetto corrente
+   * 
+   * @returns Una copia della variabile 'this.matrix'
+   */
   _copyMatrix() {
     let newMat = this._createMatrix();
     for (let i = 0; i < this.rows; i++) {
@@ -43,12 +54,24 @@ export default class Matrix {
     return newMat;
   }
 
+  /**
+   * Funzione logicamente privata che permette di scambiare la posizione di due righe nella matrice interna alla classe Matrix
+   * 
+   * @param {Number} r1 Indice della prima riga
+   * @param {Number} r2 Indice della seconda riga
+   */
   _invertiRighe(r1, r2) {
     let temp = this.matrix[r1];
     this.matrix[r1] = this.matrix[r2];
     this.matrix[r2] = temp;
   }
 
+  /**
+   * Funzione logicamente privata che permette di moltiplicare la matrice corrente per un'altra matrice
+   * 
+   * @param {Matrix} mat La matrice da moltiplicare a quella corrente
+   * @returns Un nuovo oggetto di classe Matrix che contiene il prodotto tra le due matrici
+   */
   _multPerMatrice(mat) {
     if (this.cols !== mat.rows) throw "Per il prodotto matriciale le colonne della prima matrice devono essere uguali alle righe della seconda";
     let matProdotto = this._createMatrix();
@@ -64,6 +87,12 @@ export default class Matrix {
     return new Matrix(this.rows, mat.cols, matProdotto);
   }
 
+  /**
+   * Funzione logicamente privata che permette di moltiplicare la matrice corrente per uno scalare
+   * 
+   * @param {Number} s Lo scalare da moltiplicare alla matrice corrente
+   * @returns Un nuovo oggetto di classe Matrix che contiene il prodotto tra lo scalare e la matrice corrente
+   */
   _multPerScalare(s) {
     if (!(s instanceof Fraction))
       if (isNaN(s) || s !== parseInt(s)) throw "Lo scalare deve essere un oggetto di tipo Fraction o un numero intero";
@@ -75,11 +104,13 @@ export default class Matrix {
   }
 
   /**
-   * Restituisce una nuova matrice con una riga e una colonna eliminate relativamente alla matrice inserita, che non viene modificata
+   * Funzione che elimina la riga e la colonna cui appartiene il pivot passato dalle coordinate 'i' e 'j'.
+   * La matrice 'mat' non viene modificata
    * 
-   * @param {Array} mat Matrice
-   * @param {Number} i Indice di riga da eliminare
-   * @param {Number} j Indice di colonna da eliminare
+   * @param {Array} mat La matrice in cui eliminare una riga e una colonna
+   * @param {Number} i Coordinata di riga del pivot
+   * @param {Number} j Coordinata di colonna del pivot
+   * @returns Una nuova matrice (array di array) con una riga e una colonna in meno di quella iniziale
    */
   _eliminaRigaColonna(mat, i, j) {
     let newMat = this._createMatrix(this.rows - 1);
@@ -97,6 +128,12 @@ export default class Matrix {
     return newMat;
   }
 
+  /**
+   * Funzione che permette di sommare la matrice corrente a quella passata per parametro
+   * 
+   * @param {Matrix} m La matrice da sommare a quella corrente
+   * @returns Un nuovo oggetto di classe Matrix contenente la somma tra le due matrici
+   */
   add(m) {
     if (this.rows !== m.rows || this.cols !== m.cols) throw "Le dimensioni delle matrici devono essere uguali";
     let tempM = this.matrix;
@@ -106,10 +143,34 @@ export default class Matrix {
     return new Matrix(this.rows, this.cols, tempM);
   }
 
+  /**
+   * Funzione che permette di sottrarre la matrice corrente a quella passata per parametro
+   * 
+   * @param {Matrix} m La matrice da sottrarre a quella corrente
+   * @returns Un nuovo oggetto di classe Matrix contenente la differenza tra le due matrici
+   */
   sub(m) {
     return this.add(m._multPerScalare(-1));
   }
+  
+  /**
+   * Funzione che permette di moltiplicare la matrice corrente alla matrice o allo scalare passati per parametro
+   * 
+   * @param {Matrix|Number|Fraction} x La matrice (o lo scalare) da moltiplicare a quella corrente
+   * @returns Un nuovo oggetto di classe Matrix contenente il prodotto tra le due matrici (o tra matrice e scalare)
+   */
+   mult(x) {
+    if (x instanceof Matrix)
+      return this._multPerMatrice(x);
+    else
+      return this._multPerScalare(x);
+  }
 
+  /**
+   * Funzione che calcola la trasposta della matrice corrente
+   * 
+   * @returns Un nuovo oggetto di classe Matrix contenente la trasposta della matrice corrente
+   */
   transposition() {
     let tempMatrix = this._createMatrix(this.cols);
     for (let i = 0; i < this.rows; i++) {
@@ -120,6 +181,11 @@ export default class Matrix {
     return new Matrix(this.cols, this.rows, tempMatrix);
   }
 
+  /**
+   * Funzione che calcola la forma a scala basandosi sulla matrice corrente
+   * 
+   * @returns Un nuovo oggetto di classe Matrix contenente la matrice a scala della matrice corrente
+   */
   riduzioneScala() {
     let newMat = new Matrix(this.rows, this.cols, this._copyMatrix());
     // Indica la riga del pivot
@@ -155,6 +221,11 @@ export default class Matrix {
     return newMat;
   }
 
+  /**
+   * Funzione che calcola il rango della matrice corrente
+   * 
+   * @returns Un numero intero che rappresenta il rango della matrice corrente
+   */
   rango() {
     let matrScala = this.riduzioneScala();
     let rango = 0;
@@ -169,15 +240,10 @@ export default class Matrix {
     return rango;
   }
 
-  mult(x) {
-    if (x instanceof Matrix)
-      return this._multPerMatrice(x);
-    else
-      return this._multPerScalare(x);
-  }
-
   /**
-   * Calcolo il determinante con il metodo di Laplace implementato ricorsivamente
+   * Funzione che calcola il determinante della matrice corrente utilizzando il metodo di Laplace implementato in modo ricorsivo
+   * 
+   * @returns Un oggetto di tipo Fraction contenente il determinante della matrice corrente
    */
   det() {
     if (this.rows !== this.cols) throw "Impossibile calcolare il determinante in una matrice non quadrata";
@@ -195,6 +261,11 @@ export default class Matrix {
     return sum;
   }
 
+  /**
+   * Funzione che calcola la matrice inversa a quella corrente
+   * 
+   * @returns Un nuovo oggetto di classe Matrix contenente la matrice inversa a quella corrente
+   */
   inversa() {
     if (this.rows !== this.cols) throw "Impossibile trovare la matrice inversa di una matrice non quadrata"
     let det = this.det();
@@ -210,6 +281,12 @@ export default class Matrix {
     return new Matrix(this.rows, this.cols, newMat).transposition().mult(det.reciprocal());
   }
 
+  /**
+   * Funzione che permette di comparare la matrice corrente con un'altra matrice
+   * 
+   * @param {Matrix} f La matrice a cui comparare quella corrente
+   * @returns true se le due matrici sono uguali (tutti gli elementi sono uguali), false altrimenti
+   */
   equals(m) {
     if (this.rows !== m.rows || this.cols !== m.cols) return false;
     for (let i = 0; i < this.rows; i++) {
